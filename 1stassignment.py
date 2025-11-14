0b654fddd42723ff69740a1529bcd2f9ec4f67b2
farxana jafary <farzanajafari17@gmail.com>
2025-11-14 18:47:52 +0430

    Add files via upload

---
+++ 1stassignment.py
@@ -0,0 +1,16 @@
+#assignment 1
+# Farzana Jafary 
+
+print("My name is Farzana .\n"
+      "My fun fact is that I love coding, I am able to code in flutter, dart, html, css, c++, javascript .\n"
+      "My favorite color is pink and blue.\n"
+      "My favorite drink is chai and coffee.\n"
+      "My favorite animal is a phoenix.\n")
+
+print("\"The beautiful thing about learning is that no one can take it away from you.\"\n"
+      "- B.B. King\n")
+
+print("My favorite food items are:\n"
+      "\t1. Pizza\n"
+      "\t2.pasta\n"
+      "\t3.  Ice-cream")
---
+++ 2ndassignment.py
@@ -0,0 +1,21 @@
+# Assignment 2
+# Name: Farzana Jafary
+
+
+
+day = "Friday"  
+
+
+current_year = 2025
+
+
+age = 21   
+
+
+print("Today is", day, "and this is the year", current_year, "!")
+
+
+year_of_birth = 2025 - 21
+
+
+print("I am", age, "years old. I was born in the year", year_of_birth, ".")
